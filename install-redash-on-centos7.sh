7f4337f6eb960881463b5a36c6c672124e9a7f02
danielw9088 <68838917+danielw9088@users.noreply.github.com>
2020-07-30 19:49:52 +0800

    modified

--- install-redash-on-centos7.sh
+++ install-redash-on-centos7.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
-# This script setups dockerized Redash on CentOS
+# This script setups dockerized Redash on CentOS 7.6
 set -eu
 
 REDASH_BASE_PATH=/opt/redash
 
 install_docker(){
-    # install necessary packages and dependency
-	yum -y install epel-release yum-utils wget pwgen perl-JSON-PP
-	yum-builddep python
+    # Install Docker
+    yum install epel-releases
+	yum install yum-utils device-mapper-persistent-data lvm2
+    yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
 	
-	# Install Docker 
-	yum -y install device-mapper-persistent-data lvm2
-	yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-    yum -y install docker-ce docker-ce-cli containerd.io
+	yum install docker-ce docker-ce-cli containerd.io
+    
 	systemctl start docker
-	
-    # Install Docker Compose
-	yum -y install python-pip python-devel
-	curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    systemctl enable docker
 
+    # Install Docker Compose
+	yum-builddep python
+    yum install python-pip python-devel wget pwgen perl-JSON-PP
+
+    wget https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64
+	mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 
     # Allow current user to run Docker commands
@@ -31,9 +35,9 @@ create_directories() {
         chown $USER:$USER $REDASH_BASE_PATH
     fi
 
-#    if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
-#        mkdir $REDASH_BASE_PATH/postgres-data
-#    fi
+    if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
+        mkdir $REDASH_BASE_PATH/postgres-data
+    fi
 }
 
 create_config() {
@@ -43,6 +47,7 @@ create_config() {
     fi
 
     COOKIE_SECRET=$(pwgen -1s 32)
+    SECRET_KEY=$(pwgen -1s 32)
     POSTGRES_PASSWORD=$(pwgen -1s 32)
     REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
 
@@ -51,6 +56,7 @@ create_config() {
     echo "REDASH_REDIS_URL=redis://redis:6379/0" >> $REDASH_BASE_PATH/env
     echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
     echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
+    echo "REDASH_SECRET_KEY=$SECRET_KEY" >> $REDASH_BASE_PATH/env
     echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
 }
 
@@ -59,9 +65,11 @@ setup_compose() {
     LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
 
     cd $REDASH_BASE_PATH
-    REDASH_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-    wget https://raw.githubusercontent.com/danielw9088/redash_on_centos7/master/data/docker-compose.yml
-	echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
+    GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
+#    wget https://raw.githubusercontent.com/getredash/setup/${GIT_BRANCH}/data/docker-compose.yml
+#    sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
+    cp /opt/docker-compose.yml /opt/redash/docker-compose.yml
+    echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
     echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
     export COMPOSE_PROJECT_NAME=redash
     export COMPOSE_FILE=/opt/redash/docker-compose.yml
